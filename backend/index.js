cdaf92abfc3c061878143434fad610dbe64ed58e
Arpan Barik <146699772+ArpanBarik001@users.noreply.github.com>
2025-11-08 12:41:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ passportConfig(passport);
 const port=process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:["http://localhost:3000","https://ornate-croquembouche-54541f.netlify.app"]}));
+
 
 // app.get("/",(req, res)=>{
 //     res.send("Connecting");
