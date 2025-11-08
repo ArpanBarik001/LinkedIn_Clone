d4bf60dda7d1e197779bed202a11d06424c62ec4
Arpan Barik <146699772+ArpanBarik001@users.noreply.github.com>
2025-11-08 20:21:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,22 +12,39 @@ passportConfig(passport);
 const port=process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors({origin:["http://localhost:3000","https://ornate-croquembouche-54541f.netlify.app"]}));
+app.use(cors());
 
 
 // app.get("/",(req, res)=>{
 //     res.send("Connecting");
 // })
 app.use('/user', user);
-mongoose.connect("mongodb+srv://arpan2003:arpan123@bookstore.o3puuyo.mongodb.net/").then(()=>{
-    console.log("Sucessfully connected");
-    app.listen(port, ()=>{
-    console.log(`Server is listening on port ${port}`);
-})
-}).catch((error)=>{
-    console.log(error);
-});
+// mongoose.connect("mongodb+srv://arpan2003:arpan123@bookstore.o3puuyo.mongodb.net/").then(()=>{
+//     console.log("Sucessfully connected");
+//     app.listen(port, ()=>{
+//     console.log(`Server is listening on port ${port}`);
+// })
+// }).catch((error)=>{
+//     console.log(error);
+// });
 
+let isConnect=false;
+async function connectToMongoDB(){
+    try{
+        await mongoose.connect("mongodb+srv://arpan2003:arpan123@bookstore.o3puuyo.mongodb.net/");
+        isConnect=true;
+        console.log("Connect to Mongodb");
+    }catch(error){
+        console.log(error);
+    }
+}
+
+app.use((req,res,next)=>{
+    if(!isConnect){
+        connectToMongoDB();
+    }
+    next();
+})
 export default app;
 
 
